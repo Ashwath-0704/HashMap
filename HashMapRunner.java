07bbd9777ef81f6aef501928428b725020421d1e
Achyuth Naidu <ashwath.bly@gamil.com>
2021-12-23 13:56:25 +0530

    UC3HashMap

--- HashMapRunner.java
+++ HashMapRunner.java
@@ -49,11 +49,39 @@ public class HashMapRunner {
 		}
 		System.out.println(myHashMap);
 	}
+
+	/*
+	 * UC3 Remove the word
+	 */
+	public void removeAnywords() {
+		System.out.print("Enter a string: ");
+		String message = sc.nextLine();
+//"Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+		String[] messagearray = message.toLowerCase().split(" ");
+
+		for (String word : messagearray) {
+			Integer value = myHashMap.get(word);
+//			System.out.println(value);
+			if (value == null) {
+				value = 1;
+			} else {
+				value++;
+			}
+			myHashMap.add(word, value);
+			System.out.println(myHashMap);
+			System.out.print("\nWhich word you want to remove: ");
+			String str = sc.nextLine();
+			myHashMap.remove(str);
+		}
+		System.out.println(myHashMap);
+	}
+
 	public static void main(String[] args) {
 		// creating Object
 		HashMapRunner hash = new HashMapRunner();
 		
 //		hash.findFrequency(); // UC1
-		hash.findFrequencyForLargeSentence(); // UC2
+//		hash.findFrequencyForLargeSentence(); // UC2
+		hash.removeAnywords(); // UC3
 	}
 }
